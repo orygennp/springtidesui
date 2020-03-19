ca8dc77218a6ec6d3af9b2a7143f1c972461b585
Matthew Hamilton <mahamilton@orygen.org.au>
2020-03-19 21:28:56 +1100

    v9039

--- tests/render_RMD.r
+++ tests/render_RMD.r
@@ -9,11 +9,18 @@ sim_data_r4_path_chr <- paste0(project_data_path_chr,"/sim_data_r4_4.rds")
 sim_results_ls_path_chr <- paste0(project_data_path_chr,"/sim_results_ls_4.rds")
 params_ls <- readRDS(paste0(project_data_path_chr,"/params_4.rds"))
 params_ls$data_pckg_chr <- "springtides"
+params_ls$disorder_chr <- "Any_Affective_Disorder"
+params_ls$meso2_bound_yr <- 2011
+params_ls$meso2_chr <- "0800"
+params_ls$meso2_name_chr <- "0800 Postal Area"
+params_ls$n_its_int <- 10
 params_ls$pa_r4_chr <- "aus_pa_r4"
 params_ls$r_data_dir_chr <- r_data_path_chr
 params_ls$input_ls_path_chr <- input_ls_path_chr
 params_ls$sim_data_r4_path_chr <- sim_data_r4_path_chr
 params_ls$sim_results_ls_path_chr <- sim_results_ls_path_chr
+params_ls$stat_chr <- "Prevalence - Annual"
+params_ls$title_chr <- "Springtides report"
 input <-list(report_format_chr = "HTML")
 params_ls$pdf_output_lgl <- ifelse(input$report_format_chr=="PDF",T,F)
 out <- rmarkdown::render(paste0(temp_dir_chr,'/report.Rmd'),
